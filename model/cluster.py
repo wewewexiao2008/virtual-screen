8cfe07de0668131b97704c12bcd8739134b4ae86
wewewexiao2008 <a289543261@163.com>
2022-08-28 18:54:31 +0800

    cluster v1.1

--- model/cluster.py
+++ model/cluster.py
@@ -1,17 +1,36 @@
-import datetime
-import time
+import glob
 
 import joblib
 import numpy as np
 import pandas as pd
 from rdkit import DataStructs
 from rdkit.DataStructs import ExplicitBitVect
-from sklearn.cluster import KMeans, MiniBatchKMeans, DBSCAN, AgglomerativeClustering
-from sklearn.decomposition import PCA
+from sklearn.cluster import MiniBatchKMeans
 from loguru import logger
-from sklearn.manifold import TSNE
-
+from rdkit.Chem import Draw
+from scripts.rdkit2pdbqt import MolFromPDBQTBlock
 from data.tools.utils import timing
+
+
+def show_raw_mols(df, mol_dir, group_id, out_dir):
+    mols = []
+    ids = []
+    for mol_id in df[df['layer_1'] == group_id]['id'][:]:
+        _ls = glob.glob(r'{}/**/{}.pdbqt'.format(mol_dir, mol_id), recursive=True)
+        mol_path = _ls[0]
+        with open(mol_path, 'r') as mol_f:
+            mol = MolFromPDBQTBlock(mol_f.read())
+        mols.append(mol)
+        ids.append(mol_id)
+
+    img = Draw.MolsToGridImage(
+        mols,
+        molsPerRow=4,
+        subImgSize=(200, 200),
+        legends=[i for i in ids]
+    )
+
+    img.save('{}/{}'.format(out_dir, group_id))
 
 
 class Reducer:
@@ -24,20 +43,17 @@ class Reducer:
                  batch_size: int = 10000,
                  max_iter: int = 1000,
                  init_size: int = 10000,
+                 layer: int = 0
                  ):
 
         self.n_clusters = n_clusters
         self.batch_size = batch_size
         self.max_iter = max_iter
         self.init_size = init_size
+        self.layer = layer
 
-    def run(self, fps_path):
-        """main procedure"""
-
-        """step 1: read data"""
+    def mb_kmeans(self, df, verbose=False, save=False):
         fps = []
-        df = pd.read_csv(fps_path, delimiter='\t')
-        logger.info("cluster data amount: {}".format(len(df)))
         for i in df['base64']:
             _fp = ExplicitBitVect(0)
             ExplicitBitVect.FromBase64(_fp, i)
@@ -45,7 +61,6 @@ class Reducer:
             DataStructs.ConvertToNumpyArray(_fp, arr)
             fps.append(arr)
 
-        """step 2: configure"""
         clustering = MiniBatchKMeans(n_clusters=self.n_clusters,
                                      batch_size=self.batch_size,
                                      max_iter=self.max_iter,
@@ -54,19 +69,26 @@ class Reducer:
                                      random_state=None, tol=0.0, max_no_improvement=10,
                                      n_init=3, reassignment_ratio=0.01)
 
-        """step 3: fit the data"""
-        with timing("fitting and predicting with mini-batch k-means"):
-            tic = time.time()
+        if verbose:
+            with timing("running mb-kmeans"):
+                y = clustering.fit_predict(fps)
+        else:
             y = clustering.fit_predict(fps)
-            toc = time.time()
 
-        # label
-        df_new = pd.DataFrame()
-        df_new['id'] = df['id']
-        df_new['gp1'] = y
-        df_new.to_csv(fps_path)
-        t_train = toc-tic
+        if save:
+            joblib.dump(clustering, '{}'.format())
 
+        df['layer_{}'.format(self.layer)] = y
+        return df
 
+    def run_with_fps(self, fps_path, verbose=False):
+        """main procedure"""
+        df = pd.read_csv(fps_path, delimiter='\t')
+        if verbose:
+            logger.info("cluster data amount: {}".format(len(df)))
+        return self.mb_kmeans(df, verbose=verbose)
 
-
+    def run_with_df(self, df, verbose=False):
+        if verbose:
+            logger.info("cluster data amount: {}".format(len(df)))
+        return self.mb_kmeans(df, verbose=verbose)
--- run_cluster_mpi.py
+++ run_cluster_mpi.py
@@ -1,10 +1,13 @@
 import glob
 
+from loguru import logger
+
 from model.cluster import Reducer
 import argparse
 import pandas as pd
 import numpy as np
 import mpi4py.MPI as MPI
+import os
 
 def main():
 
@@ -13,38 +16,77 @@ def main():
 
     parser.add_argument('-f', '--fps_dir', type=str, required=True)
     parser.add_argument('-o', '--out_dir', type=str, required=True)
-    parser.add_argument('-n', '--n_cpus', type=int, default=576)
 
     args = parser.parse_args()
 
     fps_dir = args.fps_dir
     out_dir = args.out_dir
-    n_cpus = args.n_cpus
-
-    reducer = Reducer(
-        n_clusters=2000,
-        batch_size=1000,
-        max_iter=1000,
-        init_size=2000
-    )
 
     comm = MPI.COMM_WORLD
     comm_rank = comm.Get_rank()
     comm_size = comm.Get_size()
     proc_name = MPI.Get_processor_name()
 
+    nc_layer1 = comm_size*2
+    nc_layer2 = comm_size*4
+
+    l1_reducer = Reducer(
+        n_clusters=nc_layer1,
+        batch_size=1000,
+        max_iter=1000,
+        init_size=nc_layer1
+    )
+    l2_reducer = Reducer(
+        n_clusters=nc_layer2,
+        batch_size=1000,
+        max_iter=1000,
+        init_size=nc_layer2
+    )
+
     root = 100
-
+    verbose = True if comm_rank == root else False
     if comm_rank == root:
-        fps_paths = glob.glob(r'out/fingerprint_mpi_new/*.fps', recursive=True)
+        log_dir = os.path.join(os.path.curdir, 'log')
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+        log_file = os.path.join(log_dir, 'debug_{time}.log')
+        logger.add(log_file)
 
+        fps_paths = glob.glob(r'{}/*.fps'.format(fps_dir), recursive=True)
         send_buf = fps_paths
     else:
         send_buf = None
 
+    # path list
     local_data = comm.scatter(send_buf, root=root)
 
-    reducer.run(local_data)
+    """Layer1"""
+    df_raw = pd.read_csv(local_data, delimiter='\t')
+    if verbose:
+        logger.info("cluster data amount: {}".format(len(df_raw)))
+    df = l1_reducer.run_with_fps(df_raw, verbose)
+
+    try:
+        df.to_csv('{}/layer1/ckpt_{}.tsv'.format(out_dir, comm_rank), sep='\t')
+    except Exception as e:
+        os.makedirs('{}/layer1'.format(out_dir))
+        df.to_csv('{}/layer1/ckpt_{}.tsv'.format(out_dir, comm_rank), sep='\t')
+
+    """Layer2"""
+    l2_dfs = []
+    for i in range(nc_layer1):
+        _df = df[df['layer_1'] == i]
+        _df = l2_reducer.run_with_df(_df, verbose)
+        l2_dfs.append(_df)
+
+    df_final = pd.concat(l2_dfs)
+    try:
+        df_final[['id', 'layer_1', 'layer_2']].to_csv('{}/layer2/ckpt_{}.tsv'.format(out_dir, comm_rank), sep='\t')
+    except Exception as e:
+        os.makedirs('{}/layer2'.format(out_dir))
+        df_final[['id', 'layer_1', 'layer_2']].to_csv('{}/layer2/ckpt_{}.tsv'.format(out_dir, comm_rank), sep='\t')
+
+
 
 
 
